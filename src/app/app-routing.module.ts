d4acd2f9c6b0e7bb551e7cf0a1cde4445b20a097
cafort <caleb.fort@gmail.com>
2018-01-11 15:58:35 -0600

    Search and sort

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FruitComponent } from "./fruit/fruit.component";
+import { FruitDetailComponent } from "./fruit-detail/fruit-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/fruits', pathMatch: 'full'},
+  {path: 'fruits', component: FruitComponent},
+  {path: 'detail/:id', component: FruitDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FruitComponent } from './fruit/fruit.component';
+import { CategoriesService } from "./shared/services/categories.service";
+import { FruitsService } from "./shared/services/fruits.service";
+import { FruitDetailComponent } from './fruit-detail/fruit-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FruitComponent,
+    FruitDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpModule,
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    CategoriesService,
+    FruitsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
